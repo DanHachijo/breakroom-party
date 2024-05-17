7e8cb67f94ce8b0cfdeb9167ca5736ec8de5ed3d
Dan Nakatoshi <60120881+DanNakatoshi@users.noreply.github.com>
2024-05-17 16:09:31 -0400

    added daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Hello</h1>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<button class="btn btn-primary">Btn</button>
 
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
