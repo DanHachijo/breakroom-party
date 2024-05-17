91e0cdb8c72b12aecbee330aba53c8761eb5721f
Dan Nakatoshi <60120881+DanNakatoshi@users.noreply.github.com>
2024-05-17 17:14:52 -0400

    added dot env

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
-<button class="btn btn-primary">Btn</button>
+<script>
+	import { get } from '$lib/supabase/client';
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+	async function fetchData() {
+		try {
+			const response = await get();
+			if (response.status === 200) {
+				console.log('Data fetched successfully:', response.body.data);
+				// Do something with the fetched data
+			} else {
+				console.error('Failed to fetch data:', response.body.error);
+			}
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+
+	fetchData();
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<button class="btn btn-primary">Btn</button>
